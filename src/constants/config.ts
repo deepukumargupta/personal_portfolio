e271d5745a92ee14dd19c1dae5940820e64e2c45
Deepu eleganza <45801491+deepukumargupta@users.noreply.github.com>
2025-09-28 02:25:56 +0400

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,12 +40,12 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Deepu Kumar Gupta Portfolio",
+    fullName: "Deepu Gupta",
+    email: "deepu855305@gmail.com",
   },
   hero: {
-    name: "John Doe",
+    name: "Deepu Kumar Gupta",
     p: ["I develop 3D visuals, user", "interfaces and web applications"],
   },
   contact: {
